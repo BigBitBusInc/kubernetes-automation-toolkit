38dd51ad506d52b14ff291b583412f2500e36d2a
root <root@golangtest2.localdomain>
2020-12-23 14:44:16 -0500

    node_modules not needed

--- code/app-code/frontend/todo-react/node_modules/semantic-ui-react/dist/commonjs/addons/MountNode/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
-exports.__esModule = true;
-
-var _MountNode = _interopRequireDefault(require("./MountNode"));
-
-exports.default = _MountNode.default;
